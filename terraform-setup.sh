36950ae65ceb2e46ae9b9232a51adc51c9a67fae
Masahito Zembutsu <m.zembutsu@gmail.com>
2017-04-06 11:11:39 +0900

    セットアップ用スクリプトの修正

--- terraform-setup.sh
+++ terraform-setup.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+mkdir ~/bin/
+mkdir ~/tmp/
+
 # Terraform
-cd /tmp
+cd ~/tmp/
 curl -L https://releases.hashicorp.com/terraform/0.9.2/terraform_0.9.2_linux_amd64.zip > terraform.zip
 unzip terraform.zip
-mv ./terraform /usr/local/bin
+mv ./terraform ~/bin
 
 
 # provider sakuracloud
 curl -L https://github.com/yamamoto-febc/terraform-provider-sakuracloud/releases/download/v0.7.3/terraform-provider-sakuracloud_linux-amd64.zip > terraform-provider-sakuracloud.zip
 unzip terraform-provider-sakuracloud.zip
-mv ./terraform-provider-sakuracloud /usr/local/bin
+mv ./terraform-provider-sakuracloud ~/bin
 
