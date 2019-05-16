bcc440b1614c2ae526984b8af2c8f2d6f28675b1
Masahito Zembutsu <zembutsu@users.noreply.github.com>
2019-05-16 14:01:19 +0900

    最新版に対応

--- terraform-setup.sh
+++ terraform-setup.sh
@@ -5,13 +5,12 @@ mkdir ~/tmp/
 
 # Terraform
 cd ~/tmp/
-curl -L https://releases.hashicorp.com/terraform/0.10.8/terraform_0.10.8_linux_amd64.zip > terraform.zip
+curl -L https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip > terraform.zip
 unzip terraform.zip
 mv ./terraform ~/bin
 
 
 # provider sakuracloud
-curl -L https://github.com/sacloud/terraform-provider-sakuracloud/releases/download/v0.14.0/terraform-provider-sakuracloud_linux-amd64.zip > terraform-provider-sakuracloud.zip
+curl -L https://github.com/sacloud/terraform-provider-sakuracloud/releases/download/v1.11.4/terraform-provider-sakuracloud_1.11.4_linux-amd64.zip > terraform-provider-sakuracloud.zip
 unzip terraform-provider-sakuracloud.zip
-mv ./terraform-provider-sakuracloud ~/bin
 
